9ba8ee77996234d0531a538916fe66d6a537c3e3
DragonGhost7 <darkiridiumghost@gmail.com>
2022-10-15 20:24:04 -0400

    no need for bin backup

--- bin_backup/cache_spotifyd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# spt playback -f "%s spotifyd" > ~/.cache/spotifyd/status
-
-spt playback -f "Album  %b
-AlbumArtist %a
-Artist  %a
-Title   %t
-%s spotifyd" | tee >( head -n -1 > ~/.cache/spotifyd/metadata) >( tail -n1 > ~/.cache/spotifyd/status)
--- bin_backup/get_amc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-amc_price=$(curl -s https://finance.yahoo.com/quote/AMC\?p\=AMC\&.tsrc\=fin-srch | grep -o '<span class="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)" data-reactid="32">[0-9].\.[0-9].</span>' | grep -o "[0-9].\.[0-9].")
-
-echo " AMC:$amc_price"
